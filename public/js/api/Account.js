b2dd84f5b2fe34737591c444263e0b44b9f073ea
Milana <durckina.milana2016@yandex.ru>
2023-02-16 23:14:22 +0300

    new commit

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -1,13 +1,18 @@
-/**
- * Класс Account наследуется от Entity.
- * Управляет счетами пользователя.
- * Имеет свойство URL со значением '/account'
- * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
+  static URL = '/account';
 
+  // Получает информацию о счёте
+  static get(id, callback){
+    createRequest({
+      url: this.URL + `/${id}`,
+      data: id,
+      method: 'GET',
+      callback: (error, response) => {
+        if (!response.success) {
+          console.log(response.error);
+        }
+        callback(error, response);
+      },
+    });
   }
 }
